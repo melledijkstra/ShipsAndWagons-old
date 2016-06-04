87ec40e8e6344ef6a72169e8bf353857e478efd6
Melle Dijkstra <melle210202@gmail.com>
2016-06-04 21:51:30 +0200

    Add project files.

---
+++ Carri-e-Navi-wpf/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Carri_e_Navi_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MouseDown += delegate { DragMove(); };
+        }
+    }
+}
